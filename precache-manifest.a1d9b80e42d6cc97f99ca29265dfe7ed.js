3f77ee5f19cbf2e7ae93cc40d7a7a5f85bb68c60
asmihaylov <mihaylov.web@gmail.com>
2019-11-08 10:27:43 +0300

    Updates

---
+++ precache-manifest.a1d9b80e42d6cc97f99ca29265dfe7ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07654152b7ec969044f207029e6f12d6",
+    "url": "/react-basics-project/index.html"
+  },
+  {
+    "revision": "589246c13469ff3e4ef2",
+    "url": "/react-basics-project/static/css/main.67fd5966.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/react-basics-project/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "589246c13469ff3e4ef2",
+    "url": "/react-basics-project/static/js/main.e01b0025.chunk.js"
+  },
+  {
+    "revision": "dd61c4723141008002da",
+    "url": "/react-basics-project/static/js/runtime-main.1d88d15a.js"
+  }
+]);
