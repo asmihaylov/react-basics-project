31b7247ef19e41bc0df3150baeda730a7c78b6d6
asmihaylov <mihaylov.web@gmail.com>
2019-11-11 17:55:57 +0300

    hot fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
-import { SearchBar, SearchBox } from './components/search-box/search-box.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
